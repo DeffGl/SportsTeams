59fd48aac6a8e15b195e214db1cf79e811b3dc7a
DeffGl <holldeevv@gmail.com>
2023-03-13 18:24:48 +0700

    Initial commit

---
+++ src/main/java/com/example/SportsTeams/SportsTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportsTeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTeamsApplication.class, args);
+	}
+
+}
