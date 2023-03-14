50bafcd235edd569b38255c0cadd75fbd2af6a10
DeffGl <holldeevv@gmail.com>
2023-03-14 14:36:14 +0700

    Исправляет ошибку сборки

--- src/main/java/com/example/SportsTeams/SportsTeamsApplication.java
+++ src/main/java/com/example/SportsTeams/SportsTeamsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportsTeamsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SportsTeamsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SportsTeamsApplication.class, args);
+    }
 
 }
